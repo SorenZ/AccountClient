5cdd52c930e5de49549a1cce99c9bf627792fcda
Hamed ZVand <sunrise.freedom@gmail.com>
2020-09-03 00:41:07 +0430

    added Template project

---
+++ MyTemplate/server/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetify;
+using DotNetify.Security;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace mytemplate
+{
+   public class Startup
+   {
+      public void ConfigureServices(IServiceCollection services)
+      {
+         // Add OpenID Connect server to produce JWT access tokens.
+         services.AddAuthenticationServer();
+
+         services.AddSignalR();
+         services.AddDotNetify();
+
+         services.AddTransient<ILiveDataService, MockLiveDataService>();
+         services.AddSingleton<IEmployeeService, EmployeeService>();
+      }
+
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         app.UseAuthentication();
+
+         app.UseWebSockets();
+         app.UseDotNetify(config =>
+         {
+            // Middleware to do authenticate token in incoming request headers.
+            config.UseJwtBearerAuthentication(new TokenValidationParameters
+            {
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthServer.SecretKey)),
+               ValidateIssuerSigningKey = true,
+               ValidateAudience = false,
+               ValidateIssuer = false,
+               ValidateLifetime = true,
+               ClockSkew = TimeSpan.FromSeconds(0)
+            });
+
+            // Filter to check whether user has permission to access view models with [Authorize] attribute.
+            config.UseFilter<AuthorizeFilter>();
+         });
+
+         if (env.IsDevelopment())
+         {
+#pragma warning disable CS0618
+            app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            {
+               HotModuleReplacement = true,
+               HotModuleReplacementClientOptions = new Dictionary<string, string> { { "reload", "true" } },
+            });
+#pragma warning restore CS0618
+         }
+
+         app.UseFileServer();
+         app.UseRouting();
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapHub<DotNetifyHub>("/dotnetify");
+            endpoints.MapFallbackToFile("index.html");
+         });
+      }
+   }
+}
---
+++ MyTemplate/typings/views/AppLayout.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface IAppLayoutProps {
+    width: string;
+}
+declare const _default: React.ComponentType<Pick<IAppLayoutProps, never> & import("@material-ui/core/withWidth").WithWidthProps>;
+export default _default;
